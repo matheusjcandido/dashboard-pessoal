8e295e3bb70783d16552315d2547298b9566eb3d
Matheus J. Candido <matheusjcandido@gmail.com>
2025-02-14 16:12:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from streamlit_plotly_events import plotly_events
 
 # Configuração da página
 st.set_page_config(
-    page_title="Dashboard CBMPR",
+    page_title="Dashboard Bombeiros PR",
     page_icon="🚒",
     layout="wide",
     initial_sidebar_state="expanded"
@@ -99,8 +99,9 @@ def load_data(file):
                 st.warning(f"Atenção: {null_dates_nasc} datas de nascimento não puderam ser convertidas.")
             if null_dates_inicio > 0:
                 st.warning(f"Atenção: {null_dates_inicio} datas de início não puderam ser convertidas.")
-                
+            
             return df
+            
         except Exception as e:
             st.error(f"Erro ao processar datas: {str(e)}")
             print("Erro detalhado ao processar datas:", e)
@@ -353,8 +354,7 @@ def main():
                 'Descrição da Unidade de Trabalho',  # coluna 9
                 cargo_column,         # coluna 10
                 'Data Início',        # coluna 13
-                'Recebe Abono Permanência',  # coluna 16
-              
+                'Recebe Abono Permanência'  # coluna 16
             ]
             
             # Preparar dados para exibição
